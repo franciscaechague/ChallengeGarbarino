190f7603d7664cc355137c998a802054cf2d4f1f
Francisca Echague <franciscaechague@gmail.com>
2018-06-17 09:27:36 -0300

    Made all prodcuts api and add new product api

--- garbarino-back/app.js
+++ garbarino-back/app.js
@@ -2,64 +2,80 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const bodyParser= require('body-parser');
 
-const getAllProducts = function(callback) {
-    MongoClient.connect('mongodb://localhost:27017/ChallengeGarbarino', function (err, client) {
-        if (err) throw err;
-        console.log("Connected successfully to mongo server");
+app.use(bodyParser.urlencoded({extended: true}));
 
-        const db = client.db('ChallengeGarbarino');
+let db = '';
 
-        insertDocuments(db, function() {
-            client.close();
-        });
+MongoClient.connect('mongodb://localhost:27017/ChallengeGarbarino', function (err, client) {
+  if (err) throw err;
+  console.log("Connected successfully to mongo server");
 
-        db.collection('products').find().toArray(function (err, result) {
-            if (err) throw err;
-            callback(result);
-        })
+  db = client.db('ChallengeGarbarino');
+
+  insertDocuments(db);
+});
+
+const getAllProducts = () => {
+  return new Promise((resolve, reject) => {
+    db.collection('products').find().toArray((err, res) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(res);
     });
+  });
 };
 
-
-const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('products');
-    // Insert some documents
-    collection.insertMany([
-        {
-            "name": "televisor 32 LED",
-            "price": 19999.99,
-            "list_price": 29999.99,
-            "brand": "SONY",
-            "category_id": 12345
-        },
-        {
-            "name": "televisor 50 4K",
-            "price": 29999.99,
-            "list_price": 39999.99,
-            "brand": "LG",
-            "category_id": 12345
-        },
-        {
-            "name": "Microsoft OFFICE 365 PERSONAL",
-            "price": 999.99,
-            "list_price": 999.99,
-            "brand": "Microsoft",
-            "category_id": 123123,
-            "virtual": true
-        }
-    ], function(err, result) {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    });
+const addNewProduct = (product) => {
+  return new Promise((resolve, reject) => {
+    db.collection('products').save(product, (err, res) => {
+      if (err) return reject(err);
+      resolve(res);
+      console.log('saved to database');
+    })
+  })
 };
 
-app.get('/', (req, res) => getAllProducts(res.send))
+const insertDocuments = function (db) {
+  // Get the documents collection
+  const collection = db.collection('products');
+  // Insert some documents
+  collection.insertMany([
+    {
+      "name": "televisor 32 LED",
+      "price": 19999.99,
+      "list_price": 29999.99,
+      "brand": "SONY",
+      "category_id": 12345
+    },
+    {
+      "name": "televisor 50 4K",
+      "price": 29999.99,
+      "list_price": 39999.99,
+      "brand": "LG",
+      "category_id": 12345
+    },
+    {
+      "name": "Microsoft OFFICE 365 PERSONAL",
+      "price": 999.99,
+      "list_price": 999.99,
+      "brand": "Microsoft",
+      "category_id": 123123,
+      "virtual": true
+    }
+  ], function (err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+  });
+};
 
+app.get('/', (req, res) => getAllProducts().then(result => res.send(result)));
 
-app.listen(8080, () => console.log('Example app listening on port 3000!')
-)
+app.post('/addProduct', (req, res) => addNewProduct(req.body).then(result => res.send(result)));
+
+app.listen(8080, () => console.log('Example app listening on port 8080!')
+);
