d399a4b0b3f0301240dfbb4fa5997713bcc20dab
AtixLabs Dev <dev@AtixLabss-MacBook-Air.local>
2018-06-18 10:52:39 -0300

    Deleted commented code

--- garbarino-front/src/App.js
+++ garbarino-front/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import SearchBar from './components/searchBar/SearchBar';
-import ProductList from "./components/productList/ProductList";
-import response from './components/FakeData';
 import ProductListContainer from './components/productList/ProductListContainer';
 
 class App extends Component {
@@ -12,7 +10,6 @@ class App extends Component {
       <div className="App">
         <Header />
         <SearchBar />
-        {/*<ProductList productList={response} />*/}
         <ProductListContainer />
       </div>
     );
